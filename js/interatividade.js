a9320c5ec9f758fc1717c0e8b8fcd26558db5cb6
João Espeleta <joaoespeleta@gmail.com>
2024-11-25 11:10:42 -0300

    terceiro commit

--- js/interatividade.js
+++ js/interatividade.js
@@ -2,13 +2,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     const button = document.querySelector(".btn-conheca-mais");
     
-    // Adiciona uma classe para uma animação no carregamento
     button.classList.add("bounce");
     
-    // Remove a classe após a animação terminar
     setTimeout(() => {
       button.classList.remove("bounce");
-    }, 1000); // 1 segundo (tempo da animação)
+    }, 1000); 
   });
 
 
@@ -16,7 +14,6 @@ document.addEventListener("DOMContentLoaded", () => {
 document.addEventListener("DOMContentLoaded", () => {
     const typingElement = document.querySelector(".typing-effect");
   
-    // Aguarda o fim da animação para manter o texto
     typingElement.addEventListener("animationend", () => {
       typingElement.setAttribute("data-typed", "true");
     });
@@ -27,34 +24,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const section = document.querySelector("#about-school");
     const icons = document.querySelectorAll(".animated-icons .icon");
   
-    // Obtém a posição da seção em relação à tela
     const sectionPosition = section.getBoundingClientRect();
     const screenHeight = window.innerHeight;
   
-    // Verifica se a seção está visível
     if (sectionPosition.top < screenHeight - 100 && sectionPosition.bottom > 100) {
       icons.forEach((icon, index) => {
         setTimeout(() => {
-          icon.style.opacity = "1"; // Mostra os ícones
-          icon.style.transform = "translateY(0)"; // Move os ícones para a posição original
-        }, index * 200); // Adiciona um pequeno delay entre os ícones
+          icon.style.opacity = "1"; 
+          icon.style.transform = "translateY(0)"; 
+        }, index * 200); 
       });
     }
   });
-  
-/*efeito rolar flashcard*/
-// Função para animar os cards quando entrarem na tela
-function checkServicesVisibility() {
-    const servicesSection = document.querySelector('#services');
-    const rect = servicesSection.getBoundingClientRect();
-    const windowHeight = window.innerHeight;
-  
-    if (rect.top <= windowHeight * 0.8) {
-      servicesSection.classList.add('visible');
-    }
-  }
-  
-  // Chama a função ao rolar e ao carregar a página
-  window.addEventListener('scroll', checkServicesVisibility);
-  window.addEventListener('load', checkServicesVisibility);
-  
